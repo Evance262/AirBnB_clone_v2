7298a28c45bc34b0063bc12e98b6b93b5bb7e9a3
Evans262 <evancechapuma62@gmail.com>
2022-04-08 22:24:49 +0200

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 from flask import Flask
 app = Flask(__name__)
+app.url_map.strict_slashes=False
 
 
 @app.route('/')
-def hello_world():
+def hello_route():
     """Program that starts a Flask web application"""
     return 'Hello HBNB!'
